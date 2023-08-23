1e1b133d2739b94f4c53db83d35755c5b73dd047
lgo <lgo@black-cats.org>
2023-08-23 21:50:51 +0200

    fix(bucket-name): use variable for bucket name

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "bucket_name" {
+  description = "S3 Bucket name for backup storage"
+  type        = string
+}
+
 variable "region" {
   description = "Region to build the storage"
   type        = string
